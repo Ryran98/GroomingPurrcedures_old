8af904d60cbbd0b8163f6a7226a9d3bcb4b3a84b
Ryan Wilson <ryan.wilson0198@gmail.com>
2022-06-30 23:11:48 +0100

    migrate old web app to new .net 6 web app

--- GroomingPurrcedures/Controllers/HomeController.cs
+++ GroomingPurrcedures/Controllers/HomeController.cs
@@ -18,6 +18,21 @@ namespace GroomingPurrcedures.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        public IActionResult Services()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
